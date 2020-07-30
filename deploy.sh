0310084a47a1e2b0e7c3c3dbf32fabe4463e1053
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-07-30 02:46:37 +0100

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd dist/tailwind-portfolio/
+git init
+git remote add origin-deploy git@github.com:MoNouri97/MoNouri97.github.io.git
+git add .
+git commit -m "new update"
+git push -uf origin-deploy master
